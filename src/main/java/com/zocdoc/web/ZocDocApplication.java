c75a01aac53bbe37906570662b1f9c6d52720731
shobhan28 <shobhan28m@gmail.com>
2023-11-29 15:06:07 +0530

    first commit

---
+++ src/main/java/com/zocdoc/web/ZocDocApplication.java
@@ -0,0 +1,13 @@
+package com.zocdoc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZocDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZocDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zocdoc/web/ZocDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zocdoc.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZocDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
