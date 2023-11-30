ff24bbeead10ab0756470785144d0c3be72115a0
shobhan28 <shobhan28m@gmail.com>
2023-11-30 16:15:55 +0530

    Implement ReviewServiceImpl providing a method for creating reviews with validation check

---
+++ src/main/java/com/zocdoc/web/Service/Impl/ReviewServiceImpl.java
@@ -0,0 +1,39 @@
+package com.zocdoc.web.Service.Impl;
+
+import com.zocdoc.web.Entity.Doctor;
+import com.zocdoc.web.Entity.Patient;
+import com.zocdoc.web.Entity.Review;
+import com.zocdoc.web.Repository.DoctorRepository;
+import com.zocdoc.web.Repository.PatientRepository;
+import com.zocdoc.web.Repository.ReviewRepository;
+import com.zocdoc.web.Service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class ReviewServiceImpl implements ReviewService {
+
+    @Autowired
+    private DoctorRepository doctorRepo;
+
+    @Autowired
+    private PatientRepository patientRepo;
+
+    @Autowired
+    private ReviewRepository reviewRepo;
+
+    @Override
+    public Review createReview(Review review) {
+        Doctor doctor = doctorRepo.findById(review.getDoctorId()).get();
+
+        Patient patient = patientRepo.findById(review.getPatentId()).get();
+
+        Review saveReview = null;
+        if (doctor != null || patient != null) {
+            saveReview = reviewRepo.save(review);
+        }
+        return saveReview;
+    }
+}
+
+
+
