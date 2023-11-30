619d040cfdc05366e6c82fc0c47bbbdbd1f1b540
shobhan28 <shobhan28m@gmail.com>
2023-11-30 13:41:41 +0530

    Implement cancelAppointment endpoint in AppointmentController with authentication checks

--- src/main/java/com/zocdoc/web/Controller/AppointmentController.java
+++ src/main/java/com/zocdoc/web/Controller/AppointmentController.java
@@ -2,14 +2,13 @@ package com.zocdoc.web.Controller;
 
 
 import com.zocdoc.web.Entity.Appointment;
+import com.zocdoc.web.Model.AppointmentKey;
 import com.zocdoc.web.Service.AppointmentService;
+import com.zocdoc.web.Service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/appointment")
@@ -17,6 +16,9 @@ public class AppointmentController {
 
     @Autowired
     private AppointmentService appointmentService;
+
+    @Autowired
+    private AuthenticationService authService;
    
     @PostMapping("/book")
     public ResponseEntity<String> bookAppointment(@RequestBody Appointment appointment){
@@ -31,6 +33,25 @@ public class AppointmentController {
             status = HttpStatus.BAD_REQUEST;
         }
         return new ResponseEntity<String>(msg, status);
+    }
+    @DeleteMapping("/cancel")
+    ResponseEntity<Void> cancelAppointment(@RequestParam String userEmail,@RequestParam String token,
+                                           @RequestBody AppointmentKey key)
+    {
+
+        HttpStatus status;
+        if(authService.authenticate(userEmail,token))
+        {
+            appointmentService.cancelAppointment(key);
+            status = HttpStatus.OK;
+        }
+        else
+        {
+            status = HttpStatus.FORBIDDEN;
+        }
+
+        return new ResponseEntity<Void>(status);
+
     }
 
 
