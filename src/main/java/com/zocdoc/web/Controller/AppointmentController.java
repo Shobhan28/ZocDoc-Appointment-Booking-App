4f39293dc53d506f84bfae9dbf907dd4d38d8397
shobhan28 <shobhan28m@gmail.com>
2023-11-30 13:18:20 +0530

    Create AppointmentController for handling appointment-related API endpoints

---
+++ src/main/java/com/zocdoc/web/Controller/AppointmentController.java
@@ -0,0 +1,10 @@
+package com.zocdoc.web.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/appoints")
+public class AppointmentController {
+}
