194f12cc5e2ef7cde2cb480601e7d02c548e9bfd
shobhan28 <shobhan28m@gmail.com>
2023-11-30 12:49:36 +0530

    Define DoctorService interface with methods for adding a doctor and retrieving appointments for a specific doctor

---
+++ src/main/java/com/zocdoc/web/Service/DoctorService.java
@@ -0,0 +1,14 @@
+package com.zocdoc.web.Service;
+
+import com.zocdoc.web.Entity.Appointment;
+import com.zocdoc.web.Entity.Doctor;
+
+import java.util.List;
+
+public interface DoctorService {
+
+    Doctor addDoctor(Doctor doctor);
+
+    List<Appointment> getMyAppointments(Long doctorId);
+    // This method retrieves a list of appointments associated with a specific doctor.
+}
