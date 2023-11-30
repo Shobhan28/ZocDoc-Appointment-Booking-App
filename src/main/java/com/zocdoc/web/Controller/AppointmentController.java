edc6f70299df117b072869807960424143d7fd20
shobhan28 <shobhan28m@gmail.com>
2023-11-30 13:23:24 +0530

    Implement AppointmentController with an endpoint for booking appointments

--- src/main/java/com/zocdoc/web/Controller/AppointmentController.java
+++ src/main/java/com/zocdoc/web/Controller/AppointmentController.java
@@ -1,10 +1,38 @@
 package com.zocdoc.web.Controller;
 
 
+import com.zocdoc.web.Entity.Appointment;
+import com.zocdoc.web.Service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/appoints")
+@RequestMapping("/api/appointment")
 public class AppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+   
+    @PostMapping("/book")
+    public ResponseEntity<String> bookAppointment(@RequestBody Appointment appointment){
+        String msg=null;
+        HttpStatus status;
+        try{
+            appointmentService.bookAppointment(appointment);
+            msg = " Appointment booked successfully";
+            status = HttpStatus.OK;
+        }catch(Exception e){
+            msg = "Book Another Appointment as this appointment is already booked";
+            status = HttpStatus.BAD_REQUEST;
+        }
+        return new ResponseEntity<String>(msg, status);
+    }
+
+
+    
 }
