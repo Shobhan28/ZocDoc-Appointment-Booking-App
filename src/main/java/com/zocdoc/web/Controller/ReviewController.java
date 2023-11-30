b507765bb1d44852dcbac4bb9416462b24f25458
shobhan28 <shobhan28m@gmail.com>
2023-11-30 16:29:40 +0530

    Implement ReviewController with endpoints for creating reviews and retrieving reviews by doctorId, including average rating calculation

---
+++ src/main/java/com/zocdoc/web/Controller/ReviewController.java
@@ -0,0 +1,55 @@
+package com.zocdoc.web.Controller;
+
+
+import com.zocdoc.web.Entity.Doctor;
+import com.zocdoc.web.Entity.Review;
+import com.zocdoc.web.Payload.DoctorDto;
+import com.zocdoc.web.Repository.DoctorRepository;
+import com.zocdoc.web.Service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+
+    @PostMapping
+    private ResponseEntity<Review> createReview(@RequestBody Review review){
+        return new ResponseEntity<>(reviewService.createReview(review), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{doctorId}")
+    public ResponseEntity<DoctorDto> getReviewByDoctorId(@PathVariable long doctorId ){
+        Doctor doctor = doctorRepository.findById(doctorId).get();
+        List<Review> reviews = reviewService.getReviewByDoctorId(doctorId);
+        int totalRatingPoints = 0;
+        int totalReviews = 0;
+        for (Review review : reviews) {
+            totalRatingPoints += review.getRating();
+            totalReviews++;
+        }
+
+        // Calculate the average rating as a percentage
+        double averageRatingPercentage = (totalRatingPoints / (double)(totalReviews * 5)) * 100;
+
+
+        DoctorDto dto=new DoctorDto();
+        dto.setDoctor(doctor);
+        dto.setReviews(reviews);
+        dto.setRatingPercentage(averageRatingPercentage);
+
+        return new ResponseEntity<>(dto,HttpStatus.OK);
+    }
+
+}
