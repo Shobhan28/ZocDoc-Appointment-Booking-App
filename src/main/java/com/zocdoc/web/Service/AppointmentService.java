0a9b1286714e6320684a83a9b3f08e2ecae3a4bc
shobhan28 <shobhan28m@gmail.com>
2023-11-30 13:43:00 +0530

    Extend AppointmentService interface with a method for canceling appointments

--- src/main/java/com/zocdoc/web/Service/AppointmentService.java
+++ src/main/java/com/zocdoc/web/Service/AppointmentService.java
@@ -1,8 +1,12 @@
 package com.zocdoc.web.Service;
 
 import com.zocdoc.web.Entity.Appointment;
+import com.zocdoc.web.Model.AppointmentKey;
 
 public interface AppointmentService {
 
     public void bookAppointment(Appointment appointment);
+
+    public void cancelAppointment(AppointmentKey key);
+
 }
