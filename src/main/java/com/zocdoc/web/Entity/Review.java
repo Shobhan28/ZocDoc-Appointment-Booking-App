b4a51b3d15121bd3e6786da1512e71ca4ad3d842
shobhan28 <shobhan28m@gmail.com>
2023-11-30 16:29:00 +0530

    Enhanced Review Entity fields

--- src/main/java/com/zocdoc/web/Entity/Review.java
+++ src/main/java/com/zocdoc/web/Entity/Review.java
@@ -1,9 +1,12 @@
 package com.zocdoc.web.Entity;
 
 import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Entity
 @Data
@@ -13,10 +16,37 @@ import lombok.NoArgsConstructor;
 public class Review {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private long doctorId;
-    private long patentId;
-    private int rating;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer reviewId;
+
+    @ManyToOne
+    private Patient patient;
+
+    @ManyToOne
+    private Doctor doctor;
+
+    @OneToOne
+    private Appointment appointment;
+
+    private String reviewContent;
+
+    private float rating;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reviewId);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Review other = (Review) obj;
+        return Objects.equals(reviewId, other.reviewId);
+    }
 
 }
